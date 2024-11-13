7df912ced644b661d144e7a4942b457ac580ed68
Zachary Bray <zachbray9@knights.ucf.edu>
2024-11-13 00:35:07 -0500

    Removed unused imports
    Reused unused imports causing warnings in deployment.

--- client/src/components/ui/projectCard.tsx
+++ client/src/components/ui/projectCard.tsx
@@ -1,7 +1,7 @@
 import { FaArrowRight, FaGithub, FaGlobe } from "react-icons/fa6";
 import Card from "../common/card";
 import LinkButton from "./LinkButton";
-import { RxGlobe, RxVideo } from "react-icons/rx";
+import { RxVideo } from "react-icons/rx";
 
 interface Props {
     title: string
