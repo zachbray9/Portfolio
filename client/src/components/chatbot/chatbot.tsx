38d599c3f8e6fd2c9474d148b64ba8199d05c4a6
Zachary Bray <zachbray9@knights.ucf.edu>
2024-11-23 02:36:54 -0500

    Got chatbot to work from client.
    Got chatbot to successfully send and receive messages to the openai api. Now I just need to finish styling it.

--- client/src/components/chatbot/chatbot.tsx
+++ client/src/components/chatbot/chatbot.tsx
@@ -6,9 +6,13 @@ import { Formik } from "formik";
 import { FiTrash } from "react-icons/fi";
 import Message from "../../models/message";
 import { GoDependabot } from "react-icons/go";
+import axios from "axios";
+import Spinner from "../common/spinner";
+import ChatbotMessage from "./chatbotMessage";
 
 export default function Chatbot() {
     const [isOpen, setIsOpen] = useState(false)
+    const [isThinking, setIsThinking] = useState(false)
     const [messages, setMessages] = useState<Message[]>([])
 
     return (
@@ -26,37 +30,49 @@ export default function Chatbot() {
             </button>
 
             <div className={`flex flex-col justify-between overflow-hidden transition-all duration-300 ease-in-out ${isOpen ? "h-96 opacity-100" : "h-0 opacity-0"}`} aria-hidden={!isOpen}>
-                <div className="overflow-y-auto p-3">
+                <div className="flex flex-col gap-4 overflow-y-auto p-3">
                     {
                         messages && messages.length > 0 ? (
-                            messages.map(message => (
-                                <div key={message.message} className={`flex ${message.isUser ? "justify-end" : "justify-start"} items-center mb-3`}>
-                                    {!message.isUser && <GoDependabot />}
-                                    <div className={`${message.isUser ? "bg-black dark:bg-white text-white dark:text-black" : "bg-white dark:bg-black"} rounded border px-3 py-2`}>
-                                        <p className="text-sm">{message.message}</p>
-                                    </div>
-                                </div>
+                            messages.map((message, index) => (
+                                <ChatbotMessage key={index} role={message.role} message={message.message}/>
                             ))
                         ) : (
                             <div className="flex flex-col justify-center items-center gap-2 text-center">
-                                <GoDependabot size={20}/>
+                                <GoDependabot size={20} />
                                 <p>Send a message to start the chat!</p>
                                 <p className="text-sm text-muted">You can ask the bot anything about me and it will help to find the answer!</p>
                             </div>
                         )
                     }
+
+                    {isThinking &&
+                        <div className="flex gap-1 text-xl text-muted">
+                            <Spinner />
+                            <span>Thinking...</span>
+                        </div>}
                 </div>
 
                 <div className="border-t dark:border-gray-800 w-full px-2 py-3">
                     <Formik
                         initialValues={{ message: '' }}
-                        onSubmit={(values, { resetForm }) => {
-                            const newMessage: Message = { isUser: true, message: values.message }
-                            setMessages([...messages, newMessage])
+                        onSubmit={async (values, { resetForm }) => {
+                            const newMessage: Message = { role: 'user', message: values.message }
+                            setMessages(prevMessages => [...prevMessages, newMessage])
+                            setIsThinking(true)
+
+                            axios.post<Message>(import.meta.env.VITE_API_URL, newMessage)
+                                .then(response => {
+                                    setMessages(prevMessages => [...prevMessages, response.data])
+                                })
+                                .catch(error => {
+                                    console.log(error)
+                                })
+
+                            setIsThinking(false)
                             resetForm()
                         }}
                     >
-                        {({ isSubmitting, handleSubmit, values }) => (
+                        {({ handleSubmit, values }) => (
                             <form onSubmit={handleSubmit} className="flex justify-center items-center gap-2 w-full">
                                 <Button
                                     className="border p-2 text-red-500 dark:border-gray-800 disabled:opacity-50 disabled:pointer-events-none"
--- client/src/models/message.ts
+++ client/src/models/message.ts
@@ -1,4 +1,4 @@
 export default interface Message {
-    isUser: boolean
+    role: string
     message: string
 }
