3fb88db43593e4d3f7d791e8d7f6ee3663c2a04e
Zachary Bray <zachbray9@knights.ucf.edu>
2024-11-24 19:02:32 -0500

    Created static file handlers for api.
    Created static file handlers for serving the client and created a new production version of the client.

--- api/controllers/handlers.go
+++ api/controllers/handlers.go
@@ -1,7 +1,31 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterEndpoints(server *gin.Engine) {
 	server.POST("/api/chat", CallAssistantsApi)
+
+	if os.Getenv("MODE") == "production" {
+
+		server.Static("/assets", "./wwwroot/assets")
+
+		server.StaticFile("/android-chrome-192x192.png", "./wwwroot/android-chrome-192x192.png")
+		server.StaticFile("/apple-touch-icon.png", "./wwwroot/apple-touch-icon.png")	
+		server.StaticFile("/favicon-16x16.png", "./wwwroot/favicon-16x16.png")
+		server.StaticFile("/favicon-32x32.png", "./wwwroot/favicon-32x32.png")
+		server.StaticFile("/favicon.ico", "./wwwroot/favicon.ico")
+		server.StaticFile("/site.webmanifest", "./wwwroot/site.webmanifest")
+		server.StaticFile("/ChakraUiLogo.png", "./wwwroot/ChakraUiLogo.png")
+		server.StaticFile("/ZacharyBray-Resume-2024.pdf", "./wwwroot/site.ZacharyBray-Resume-2024.pdf")
+		server.StaticFile("/Zuko_Pic.jpg", "./wwwroot/Zuko_Pic.jpg")
+
+		server.NoRoute(func (c *gin.Context){
+			c.File("./wwwroot/index.html")
+		})
+
+	}
 }
