dbc957124af9effb266d1e2ebaddfa35c79fe7f1
Zachary Bray <zachbray9@knights.ucf.edu>
2025-07-02 17:06:40 -0400

    Added component style overrides for react markdown
    Added some component styling overrides for headers for react markdown because chatbot was returning headers that were too big.

--- client/src/components/chatbot/chatbotMessage.tsx
+++ client/src/components/chatbot/chatbotMessage.tsx
@@ -1,5 +1,5 @@
 import { RiRobot2Line } from "react-icons/ri"
-import Markdown from "react-markdown"
+import Markdown, { Components } from "react-markdown"
 import remarkGfm from "remark-gfm"
 
 interface Props {
@@ -7,12 +7,19 @@ interface Props {
     message: string
 }
 
+const componentOverrides: Partial<Components> = {
+    h1: (...props) => <h1 className="font-bold text-xl" {...props}/>,
+    h2: (...props) => <h2 className="font-semibold text-lg" {...props}/>,
+    h3: (...props) => <h3 className="font-medium text-base" {...props}/>,
+    h4: (...props) => <h4 className="font-medium text-sm" {...props}/>
+}
+
 export default function ChatbotMessage({ role, message }: Props) {
     if (role === "user") {
         return (
             <div className="flex justify-end items-center ">
                 <div className="text-sm bg-black dark:bg-white text-white dark:text-black rounded border px-3 py-2 max-w-64">
-                    <Markdown className='chat-message' remarkPlugins={[remarkGfm]}>{message}</Markdown>
+                    <Markdown className='chat-message' remarkPlugins={[remarkGfm]} components={componentOverrides}>{message}</Markdown>
                 </div>
             </div>
         )
@@ -22,7 +29,7 @@ export default function ChatbotMessage({ role, message }: Props) {
         <div className="flex justify-start items-center gap-2">
             <RiRobot2Line size={20} className="shrink-0" />
             <div className="text-sm bg-white dark:bg-bg-dark text-black dark:text-white rounded border dark:border-gray-800 px-3 py-2 max-w-64">
-                <Markdown className='chat-message' remarkPlugins={[remarkGfm]}>{message}</Markdown>
+                <Markdown className='chat-message' remarkPlugins={[remarkGfm]} components={componentOverrides}>{message}</Markdown>
             </div>
         </div>
     )
